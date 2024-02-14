df896a43f0854036b3296c53ecedb70cbbacfb45
dopelyner <dalyner@protonmail.com>
2024-02-14 17:27:25 -0100

    fix: stars background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas
+} from './components'
 
 const App = () => {
   return (
@@ -9,6 +18,7 @@ const App = () => {
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
+          <StarsCanvas />
         </div>
         <About />
         <Experience />
@@ -20,7 +30,7 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,46 @@
-import { motion } from "framer-motion";
-
-import { styles } from "../styles";
-import { ComputerCanvas } from "./canvas";
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { StarsCanvas } from './canvas'
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row justify-center items-center gap-5`} >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div />
-        </div>
-
+      <div
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row justify-center items-center gap-5`}
+      >
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#A27B5C]'>Tiago Lino</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Fullstack developer, passionate about tech<br className='sm:block hidden' />
+            <p className='flex justify-end mb-6 italic'>
+              aka
+              <a
+                href='https://github.com/dopelyner'
+                target='_blank'
+                className='text-[#A27B5C] pl-2 hover:underline hover:scale-105'
+              >
+                dopelyner
+              </a>
+            </p>
+            Fullstack developer, passionate about tech
+            <br className='sm:block hidden' />
             and web3 enthusiast.
           </p>
         </div>
       </div>
-
-      {/* <ComputerCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
-                y: [0, 24, 0],
+                y: [0, 24, 0]
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: "loop",
+                repeatType: 'loop'
               }}
               className='w-2 h-2 rounded-full bg-secondary mb-1'
             />
@@ -42,7 +48,7 @@ const Hero = () => {
         </a>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
