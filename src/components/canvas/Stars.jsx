6f5c75b01b4b69b21fb9de54bd0a4dd40989a701
dopelyner <dalyner@protonmail.com>
2024-05-23 09:09:18 +0000

    added analytics

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() => random.inSphere(new Float32Array(8000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 80;
@@ -18,7 +18,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color='#C06014'
-          size={0.001}
+          size={0.0007}
           sizeAttenuation={true}
           depthWrite={false}
         />
